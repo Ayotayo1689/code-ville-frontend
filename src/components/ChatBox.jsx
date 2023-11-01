812374e4b760f05f1aa0b08221fad9d3c450b11c
Ayotayo <ayotayobadejo@gmail.com>
2023-11-01 23:15:57 +0100

    fixed image issue

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,51 +1,143 @@
-import React, { useEffect, useRef, useState } from 'react'
-import Message from './Message'
-import { collection, query, where, onSnapshot, QuerySnapshot, orderBy, limit } from "firebase/firestore"
-import { db } from '../firebase'
-import "./Navbar.css";
+// import React, { useEffect, useRef, useState } from 'react'
+// import Message from './Message'
+// import { collection, query, where, onSnapshot, QuerySnapshot, orderBy, limit } from "firebase/firestore"
+// import { db } from '../firebase'
+// import "./Navbar.css";
 
 
  
-const ChatBox = () => {
-    const [messages, setMessages] = useState([]);
-    const messagesEndRef = useRef();
+// const ChatBox = () => {
+//     const [messages, setMessages] = useState([]);
+//     const messagesEndRef = useRef();
     
-    const scrollToBottom = () => {
-        messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
-    };
+//     const scrollToBottom = () => {
+//         messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+//     };
     
-    useEffect(() => {
-        scrollToBottom();
-    }, [messages]);
+//     useEffect(() => {
+//         scrollToBottom();
+//     }, [messages]);
 
    
-   useEffect(()=>{
+//    useEffect(()=>{
+//     const q = query(collection(db, "messages"),
+//     orderBy("date"),
+//     limit(50)
+    
+//     );
+//     const unsubscribe = onSnapshot(q, (QuerySnapshot)=>{
+//         const messages = [];
+//         QuerySnapshot.forEach((doc)=>{
+//             console.log(doc.data()); 
+//             messages.push({...doc.data(),id: doc.id});
+//         });
+//         setMessages(messages)
+//     })
+
+// return ()=> unsubscribe;
+
+//    }, [])
+    
+//   return (
+//     <div className='message-big' style={{padding:"20px 10px"}} >
+//         {messages.map(message =>(
+//             <Message key={message.id} message={message} />
+//         ))}
+//         <div ref={messagesEndRef}></div>
+//     </div>
+//   )
+// }
+
+// export default ChatBox
+
+
+
+
+
+import React, { useEffect, useRef, useState } from 'react';
+import Message from './Message';
+import axios from 'axios';
+import Staff from "../images/profile-icon.jpg";
+import "../pages/DashApp.css";
+import { db } from '../firebase';
+import { collection, query, onSnapshot, orderBy, limit } from "firebase/firestore";
+import { Base_url } from './Url';
+
+const ChatBox = () => {
+  const [messages, setMessages] = useState([]);
+  const [userProfiles, setUserProfiles] = useState([]);
+  const [userData, setUserData] = useState(null);
+  const messagesEndRef = useRef();
+
+  const loggedInUser = localStorage.getItem("loggedInUser");
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const fetchAllUsers = async () => {
+    const url = `${Base_url()}/users`;
+
+    axios.get(url)
+      .then(response => {
+        setUserProfiles(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching user profiles:', error);
+      });
+  };
+
+  const fetchCurrentUser = async () => {
+    const url = `${Base_url()}/users/${loggedInUser}`;
+
+    axios.get(url)
+      .then(response => {
+        setUserData(response.data.data);
+      })
+      .catch(error => {
+        console.error('Error fetching current user data:', error);
+      });
+  };
+
+  useEffect(() => {
+    fetchAllUsers();
+    fetchCurrentUser();
+  }, []);
+
+  useEffect(() => {
     const q = query(collection(db, "messages"),
-    orderBy("date"),
-    limit(50)
-    
+      orderBy("date"),
+      limit(50)
     );
-    const unsubscribe = onSnapshot(q, (QuerySnapshot)=>{
-        const messages = [];
-        QuerySnapshot.forEach((doc)=>{
-            console.log(doc.data()); 
-            messages.push({...doc.data(),id: doc.id});
-        });
-        setMessages(messages)
-    })
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const messages = [];
+      querySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
 
-return ()=> unsubscribe;
+    return () => unsubscribe();
+  }, []);
 
-   }, [])
-    
   return (
-    <div className='message-big' style={{padding:"20px 10px"}} >
-        {messages.map(message =>(
-            <Message key={message.id} message={message} />
-        ))}
-        <div ref={messagesEndRef}></div>
+    <div className='message-big' style={{ padding: "20px 10px" }}>
+      {messages.map(message => (
+        <Message
+          key={message.id}
+          message={message}
+          userProfiles={userProfiles}
+          userData={userData}
+          loggedInUser={loggedInUser}
+        />
+      ))}
+      <div ref={messagesEndRef}></div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatBox
+export default ChatBox;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,69 +1,131 @@
-import React, { useEffect, useState } from 'react'
-import Staff from "../images/profile-icon.jpg"
-import "../pages/DashApp.css"
-import axios from 'axios'
-import { Base_url } from './Url'
+// import React, { useEffect, useState } from 'react'
+// import Staff from "../images/profile-icon.jpg"
+// import "../pages/DashApp.css"
+// import axios from 'axios'
+// import { Base_url } from './Url'
 
-const Message = ({message}) => {
-    const [data, setData] = useState([]);
+// const Message = ({message}) => {
+//     const [data, setData] = useState([]);
+//     const [userProfiles, setUserProfiles ] = useState([]);
 
+//     const loggedInUser = localStorage.getItem("loggedInUser")
+//     const userText = message
+//     // userProfiles = get all users from api
+//     const currentUserID = loggedInUser
 
-
-  const loggedInUser = localStorage.getItem("loggedInUser")
-  const fetchCoinData = async () => {
+//     const fetchAlluser = async () => {
 
    
 
-  const url = `${Base_url()}/users/${loggedInUser}`
+//         const url = `${Base_url()}/users`
+      
+      
+      
+//         axios.get(url)
+//         .then(response => {
+//             setUserProfiles(response.data) 
+//         })
+//         .catch(error => {
+//         });
+      
+//       };
+
+
+//   const fetchCoinData = async () => {
+
+   
+
+//   const url = `${Base_url()}/users/${loggedInUser}`
 
 
 
-  axios.get(url)
-  .then(response => {
-  // setLoading(false)
-  setData(response.data.data)
-  })
-  .catch(error => {
-  });
+//   axios.get(url)
+//   .then(response => {
+//   // setLoading(false)
+//   setData(response.data.data)
+//   })
+//   .catch(error => {
+//   });
 
-};
+// };
 
-useEffect(() => {
+// useEffect(() => {
+  
+//     fetchAlluser()
+//     fetchCoinData();
+  
+// }, []);
+
   
 
-  fetchCoinData();
-  
-}, []);
 
-  
+//   return (
+//     <div className='chat-big'>
+//         <div className={message.senderId === loggedInUser ? "chat sender" : "chat receiver"}>
+//             <div className="chat-image avatar">
+//                 <div className="rounded-full">
+//                     <img src={message.senderId === loggedInUser  ? data.profilePic : message.pic } alt=""  style={{
+//     borderRadius: "50%",
+//     objectFit: "cover", 
+//     // maxWidth: "100%",   
+//     // maxHeight: "100%"   
+//   }} /> 
+//                </div>
+//             </div>
+//            <div className="content">
+//            <div className="chat-header">
+//                {message.name}
+//                 <time>{message.createdAt}</time>
+//             </div>
+//             <div className="chat-bubble">{message.text}</div>
+//             {/* <div className="chat-footer">
+//                 Delivered
+//             </div> */}
+//            </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Message
 
 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Staff from "../images/profile-icon.jpg";
+import "../pages/DashApp.css";
+
+const Message = ({ message, userProfiles, userData, loggedInUser }) => {
+    // console.log(userProfiles)
   return (
     <div className='chat-big'>
-        <div className={message.senderId === loggedInUser ? "chat sender" : "chat receiver"}>
-            <div className="chat-image avatar">
-                <div className="rounded-full">
-                    <img src={data.profilePic ? data.profilePic : Staff} alt=""  style={{
-    borderRadius: "50%",
-    objectFit: "cover", 
-    // maxWidth: "100%",   
-    // maxHeight: "100%"   
-  }} /> 
-               </div>
-            </div>
-           <div className="content">
-           <div className="chat-header">
-               {message.name}
-                <time>{message.createdAt}</time>
-            </div>
-            <div className="chat-bubble">{message.text}</div>
-            {/* <div className="chat-footer">
-                Delivered
-            </div> */}
-           </div>
+      <div className={message.senderId === loggedInUser ? "chat sender" : "chat receiver"}>
+        <div className="chat-image avatar">
+          <div className="rounded-full">
+            <img
+              src={
+                message.senderId === loggedInUser 
+                  ? (userData && userData.profilePic) || Staff // Use Staff image if data is not available
+                  : (userProfiles.find(user => user.id === message.senderId)?.profilePic) || Staff 
+              }
+              alt=""
+              style={{
+                borderRadius: "50%",
+                objectFit: "cover",
+              }}
+            />
+          </div>
         </div>
+        <div className="content">
+          <div className="chat-header">
+            {message.name}
+            <time>{message.createdAt}</time>
+          </div>
+          <div className="chat-bubble">{message.text}</div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
